4e85cd7e3555479d8642b54ce8a9d33b9dfbc881
ChrlB <johnbonaobra106@gmail.com>
2025-11-12 09:52:55 +0800

    added every images for each sdg

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const show_navBtn = document.getElementById("show_navBtn");
 const sidenav = document.getElementById("sidenav");
 
 hide_navBtn.addEventListener("click", ()=>{//alert("in")
- sidenav.style.width = "0"
+ sidenav.style.width = "0";
 })
 
 show_navBtn.addEventListener("click", ()=>{//alert("in")
