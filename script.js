e1513f9315c5f795cfe121041a72df16522a6a64
ChrlB <johnbonaobra106@gmail.com>
2025-11-13 11:46:03 +0800

    finalizing the project

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ const main = document.getElementById("main");
 
 hide_navBtn.addEventListener("click", ()=>{//alert("in")
  sidenav.style.width = "0";
-})
+});
 
 show_navBtn.addEventListener("click", ()=>{//alert("in")
  sidenav.style.width = "30%";
-})
+});
+
 sidenav.addEventListener("mouseleave", ()=>{//alert("in")
  sidenav.style.width = "0";
-})
+});
